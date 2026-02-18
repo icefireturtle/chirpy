0b2f949930ed174955ebe1142394d442bd2838ed
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2026-02-17 21:26:51 -0600

    rebuilt main for proper server configuration

--- main.go
+++ main.go
@@ -1,25 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type apiHandler struct {}
-
-func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
-
 func main() {
 
-	mux  := http.NewServeMux()
-	mux.Handle("/api/", apiHandler{})
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		
-		if req.URL.Path != "/" {
-			http.NotFound(w, req)
-			return
-		}
-		fmt.Fprintf(w, "Welcome to the home page!")
-	})
+	mux := http.NewServeMux()
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	s.ListenAndServe()
 
 }
